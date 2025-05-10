ba2f1f535b9e1d668983b87b77014f4b8e262a24
outlandi-co <outlandico@gmail.com>
2025-05-10 12:14:15 -0700

    updated _Redirects and App.jsxl

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Component imports
 import Navbar from './components/Navbar';
 import About from './components/About';
-import Home from './pages/Home';
-import Gallery from './components/Gallery';
 import Contact from './components/Contact';
+import Gallery from './components/Gallery';
+
+// Page imports
+import Home from './pages/Home';
 
 export default function App() {
   return (
     <Router>
-      <Navbar /> {/* ✅ Only one instance here */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      </Router>
+    </Router>
   );
 }
